e010f4fc4d3eeaaf9c7c39ef9e71414f71f314b8
Hajarfallaki <hajarfallaki24@gmail.com>
2025-05-19 08:56:39 +0100

    premier commit

---
+++ src/main/java/org/sid/examen_final_jee/ExamenFinalJeeApplication.java
@@ -0,0 +1,13 @@
+package org.sid.examen_final_jee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalJeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/examen_final_jee/ExamenFinalJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.examen_final_jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
